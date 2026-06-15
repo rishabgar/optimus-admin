26a1ac351fc3ccd6260ab7eed4df04ec1361b9d4
rishabgar <rishabg546@gmail.com>
2026-06-15 22:56:24 +0530

    nfgjbnbjgfnbjfgb

--- src/auth/axios.js
+++ src/auth/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_LOCAL_API_BASE_URL,
-  // baseURL: import.meta.env.VITE_API_BASE_URL,
+  // baseURL: import.meta.env.VITE_LOCAL_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 
   headers: {
     "Content-Type": "application/json",
